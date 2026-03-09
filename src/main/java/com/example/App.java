582f00e168c94c95c57037b2bcdaf94a6002d9f6
tnkushal <tnkushal@gmail.com>
2026-03-09 10:00:53 +0530

    first commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,39 +1,56 @@
 package com.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class App {
+
     public static void main(String[] args) {
 
-        // Selenium Firefox options
         FirefoxOptions options = new FirefoxOptions();
 
-        // Point to the real Firefox binary inside Snap
-        options.setBinary("/snap/firefox/current/usr/lib/firefox/firefox");
+        // Firefox binary path
+        options.setBinary("/home/vboxuser/devops/MyMavenSeleniumFirefoxApp04/firefox/firefox");
 
-        // GUI mode: do NOT use headless
-        // options.addArguments("-headless"); // <-- commented out to see browser
-
-        // Initialize driver
         WebDriver driver = new FirefoxDriver(options);
 
-        // Open SauceDemo and print page title
-        driver.get("https://www.saucedemo.com/");
-        System.out.println("Page title: " + driver.getTitle());
-
-        // Maximize window
-        driver.manage().window().maximize();
-
-        // Pause to see the browser for 15 seconds
         try {
-            Thread.sleep(15000); // 15 seconds
-        } catch (InterruptedException e) {
+
+            // --------------------
+            // TAB 1
+            // --------------------
+            driver.get("https://www.saucedemo.com/");
+            driver.manage().window().maximize();
+
+            driver.findElement(By.id("user-name")).sendKeys("standard_user");
+            driver.findElement(By.id("password")).sendKeys("secret_sauce");
+            driver.findElement(By.id("login-button")).click();
+
+            System.out.println("Tab 1: SauceDemo login successful");
+
+            // --------------------
+            // TAB 2
+            // --------------------
+            driver.switchTo().newWindow(WindowType.TAB);
+            driver.get("https://www.google.com");
+
+            System.out.println("Tab 2: Google opened");
+
+            // --------------------
+            // TAB 3
+            // --------------------
+            driver.switchTo().newWindow(WindowType.TAB);
+            driver.get("https://www.github.com");
+
+            System.out.println("Tab 3: GitHub opened");
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        // Quit driver
-        driver.quit();
+        // Browser will remain open
     }
 }
